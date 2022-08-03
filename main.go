1f26caf81b1cacb88b0bbc2c4b03be9249f1714b
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-08-03 19:35:59 +0200

    Separate module added.

--- main.go
+++ main.go
@@ -1,45 +1,17 @@
 package main
 
 import (
-	"errors"
+	"Go-Functions/simplemath"
 	"fmt"
-	"math"
 )
 
 func main() {
-	answer, err := divide(6, 3)
+	answer, err := simplemath.Divide(6, 3)
 	if err != nil {
 		fmt.Printf("An errors occurred %s\n", err.Error())
 	} else {
 		fmt.Printf("%f\n", answer)
 	}
-	total := sum(12.2, 14, 16, 22.4)
+	total := simplemath.Sum(12.2, 14, 16, 22.4)
 	fmt.Printf("total of sum: %f\n", total)
-}
-
-func sum(values ...float64) float64 {
-	total := 0.0
-	for _, value := range values {
-		total += value
-	}
-	return total
-}
-
-func divide(p1, p2 float64) (float64, error) {
-	if p2 == 0 {
-		return math.NaN(), errors.New("cannot divide by zero")
-	}
-	return p1 / p2, nil
-}
-
-func add(p1, p2 float64) float64 {
-	return p1 + p2
-}
-
-func subtract(p1, p2 float64) float64 {
-	return p1 - p2
-}
-
-func multiply(p1, p2 float64) float64 {
-	return p1 * p2
 }
