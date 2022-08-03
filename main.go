366a0c3287e1787fc8cae0ec02d01decb0eb67ef
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-08-03 10:39:48 +0200

    Simple function used.

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Printf("%f\n", add(2, 4))
+}
+
+func add(p1, p2 float64) float64 {
+	return p1 + p2
+}
+
+func subtract(p1, p2 float64) float64 {
+	return p1 - p2
+}
+
+func multiply(p1, p2 float64) float64 {
+	return p1 * p2
 }
