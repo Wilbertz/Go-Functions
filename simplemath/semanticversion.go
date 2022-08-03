38c7d1893ffdd4354646ed505edc38b50d4f6954
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-08-03 20:09:02 +0200

    SemanticVersion struct added.

---
+++ simplemath/semanticversion.go
@@ -0,0 +1,13 @@
+package simplemath
+
+type SemanticVersion struct {
+	major, minor, patch int
+}
+
+func NewSemanticVersion(major, minor, patch int) SemanticVersion {
+	return SemanticVersion{
+		major: major,
+		minor: minor,
+		patch: patch,
+	}
+}
