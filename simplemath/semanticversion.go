80e78f3bb54d50a9ba5589c09323213b0a542b94
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-08-05 17:49:50 +0200

    Increment methods added.

--- simplemath/semanticversion.go
+++ simplemath/semanticversion.go
@@ -16,3 +16,15 @@ func NewSemanticVersion(major, minor, patch int) SemanticVersion {
 func (sv SemanticVersion) String() string {
 	return fmt.Sprintf("%d.%d.%d", sv.major, sv.minor, sv.patch)
 }
+
+func (sv SemanticVersion) IncrementMajor() {
+	sv.major += 1
+}
+
+func (sv SemanticVersion) IncrementMinor() {
+	sv.minor += 1
+}
+
+func (sv SemanticVersion) IncrementPatch() {
+	sv.patch += 1
+}
