c65877768159b1c9dc997dcfb2f9def1f5b96d0a
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-08-05 19:16:18 +0200

    Work in progress.

--- main.go
+++ main.go
@@ -2,12 +2,31 @@ package main
 
 import "Go-Functions/simplemath"
 
+type MathExpr = string
+
+const (
+	AddExpr      = MathExpr("add")
+	SubtractExpr = MathExpr("subtract")
+	MultiplyExpr = MathExpr("multiply")
+)
+
 func main() {
-	addExpr := mathExpression()
+	addExpr := mathExpression(MultiplyExpr)
 
 	println(addExpr(2, 3))
 }
 
-func mathExpression() func(float64, float64) float64 {
-	return simplemath.Add
+func mathExpression(expr MathExpr) func(float64, float64) float64 {
+	switch expr {
+	case AddExpr:
+		return simplemath.Add
+	case SubtractExpr:
+		return simplemath.Subtract
+	case MultiplyExpr:
+		return simplemath.Multiply
+	default:
+		return func(f1 float64, f2 float64) float64 {
+			return 0
+		}
+	}
 }
